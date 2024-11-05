1282db68c3b01d53ed2a5e07e2d31d0fc4c41c26
Judoit <judoit@gmail.com>
2024-11-05 18:32:50 +0700

    change2

--- js/slick_Sign_Out.js
+++ js/slick_Sign_Out.js
@@ -1,9 +1,9 @@
 var isUserProfileVisible = localStorage.getItem("userProfileVisible");
 
-  if (isUserProfileVisible === "true") {
-       document.getElementById("userProfile").style.display = "none";
-       document.getElementById("userGreeting").style.display = "block";
-   } else if(isUserProfileVisible === "false") {
-       document.getElementById("userProfile").style.display = "block";
-       document.getElementById("userGreeting").style.display = "none";
-   }
+if (isUserProfileVisible === "true") {
+  document.getElementById("userProfile").style.display = "none";
+  document.getElementById("userGreeting").style.display = "block";
+} else {
+  document.getElementById("userProfile").style.display = "block";
+  document.getElementById("userGreeting").style.display = "none";
+}
